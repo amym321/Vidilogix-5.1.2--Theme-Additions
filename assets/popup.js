4fdea6a163d47420e1a3cbeb271fbbe5e9a4a959
Amy Mick <66137093+amym321@users.noreply.github.com>
2022-03-25 18:20:23 -0500

    Why Buy popup style update

--- assets/popup.js
+++ assets/popup.js
@@ -3,13 +3,20 @@ window.onload = function(){
   var modal = document.getElementById("why-buy-popup");
   var btn = document.getElementById("why-buy-btn");
   var span = document.getElementsByClassName("close-why-popup")[0];
+  var chat1 = document.getElementById("dummy-chat-button-iframe");
+  var chat2 = document.getElementById("shopify-chat-dummy");
+  
 
   btn.onclick = function() {
     modal.style.display = "flex";
+    chat1.style.display = "none";
+    chat2.style.display = "none";
   }
 
   span.onclick = function() {
     modal.style.display = "none";
+    chat1.style.display = "flex";
+    chat2.style.display = "flex";
   }
 
 }
