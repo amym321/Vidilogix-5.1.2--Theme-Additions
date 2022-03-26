98468a901a987b7270a6aae20474a6c5d22c2623
Amy Mick <66137093+amym321@users.noreply.github.com>
2022-03-26 16:08:36 -0500

    Why Buy popup - close on click outside of modal

--- assets/popup.js
+++ assets/popup.js
@@ -1,22 +1,29 @@
 window.onload = function(){ 
 
-  var modal = document.getElementById("why-buy-popup");
+  var outerModal = document.getElementById("why-buy-popup");
   var btn = document.getElementById("why-buy-btn");
-  var span = document.getElementsByClassName("close-why-popup")[0];
-  var chat1 = document.getElementById("dummy-chat-button-iframe");
-  var chat2 = document.getElementById("shopify-chat-dummy");
-  
 
   btn.onclick = function() {
-    modal.style.display = "flex";
-    chat1.style.display = "none";
-    chat2.style.display = "none";
+    outerModal.style.display = "flex";
   }
 
-  span.onclick = function() {
-    modal.style.display = "none";
-    chat1.style.display = "flex";
-    chat2.style.display = "flex";
+  document.addEventListener(
+    "click",
+    function(event) {
+      // If user either clicks X button OR clicks outside the modal window, then close modal 
+      if (
+        event.target.matches(".close-why-popup") ||
+        !event.target.closest("#why-modal-inner") && 
+        event.target.matches("#why-outer-modal") 
+      ) {
+        closeModal()
+      }
+    },
+    false
+  )
+  
+  function closeModal() {
+    outerModal.style.display = "none";
   }
 
 }
