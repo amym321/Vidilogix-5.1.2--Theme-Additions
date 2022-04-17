af90a2464c61b23cab1582c8e61c4b6f6e3f98c5
Amy Mick <66137093+amym321@users.noreply.github.com>
2022-04-17 12:46:08 -0500

    Media Drawer

--- assets/popup.js
+++ assets/popup.js
@@ -7,6 +7,10 @@ window.onload = function(){
   var mapModal = document.getElementById("map-popup");
   var mapBtn = document.getElementById("map-btn");    // trigger
   var mapBtn2 = document.getElementById("map-btn2");  // trigger
+  // Media Drawer
+  var mediaDrawer = document.getElementById("media-drawer");
+  var mediaDrwrOverlay = document.getElementById("media-drwr-overlay");
+  var mediaBtn = document.getElementById("media-btn");    // trigger
 
   if (btn != null){
     btn.onclick = function() {
@@ -23,6 +27,14 @@ window.onload = function(){
   if (mapBtn2 != null){
     mapBtn2.onclick = function() {
       mapModal.style.display = "flex";
+    }
+  }
+
+  if (mediaBtn != null){
+    mediaBtn.onclick = function() {
+      mediaDrawer.style.display = "block";
+      mediaDrawer.classList.add('drawer--is-open');
+      mediaDrwrOverlay.style.display = "block";
     }
   }
   
@@ -47,6 +59,10 @@ window.onload = function(){
     }
     if (mapModal != null){
       mapModal.style.display = "none";
+    }
+    if (mediaDrawer != null){
+      mediaDrawer.style.display = "none";
+      mediaDrwrOverlay.style.display = "none";
     }
   }
 
