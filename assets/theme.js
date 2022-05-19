73ce53cb0ea52491c4c61b90bc3021473a9cb1b8
Amy Mick <66137093+amym321@users.noreply.github.com>
2022-05-19 11:57:22 -0500

    open/close Recently Viewed  Search bar JS

--- assets/theme.js
+++ assets/theme.js
@@ -4585,17 +4585,7 @@ lazySizesConfig.expFactor = 4;
       var container = document.querySelector(selectors.searchContainer);
       theme.utils.prepareTransition(container, function() {
         container.classList.add('is-active');
-        // // add Recently Viewed modal under Search bar when there's history - am
-        // displayRecentView();
       }.bind(this));
-
-      // function displayRecentView() {
-      //   if (Object.keys(theme.recentlyViewed.recent).length === 0 && theme.recentlyViewed.recent.constructor === Object) {
-      //     // No previous history on page load, so bail & don't display modal
-      //     return;
-      //   }
-      //   document.getElementById("recent-view-popup").style.display = "flex";
-      // }
   
       document.documentElement.classList.add('js-drawer-open', 'js-drawer-open--search');
   
@@ -4643,9 +4633,6 @@ lazySizesConfig.expFactor = 4;
       document.documentElement.classList.add('js-drawer-closing');
       document.documentElement.classList.remove('js-drawer-open', 'js-drawer-open--search');
       
-      // // close Recently Viewed modal when Search bar closes - am
-      // document.getElementById("recent-view-popup").style.display = "none";
-
       window.setTimeout(function() {
         document.documentElement.classList.remove('js-drawer-closing');
       }.bind(this), 500);
@@ -4869,9 +4856,6 @@ lazySizesConfig.expFactor = 4;
         var resultCount = 0;
   
         cache.wrapper.classList.remove('hide');
-
-        // // close Recently Viewed modal when Search results display - am
-        // document.getElementById("recent-view-popup").style.display = "none";
 
         var resultTypes = Object.entries(suggestions.resources.results);
   
@@ -8269,115 +8253,5 @@ lazySizesConfig.expFactor = 4;
 
     document.dispatchEvent(new CustomEvent('page:loaded'));
   });
-
-  // // Popup modals - am
-  
-  // function customModals(){ 
-
-  //   // Why Buy popup
-  //   var outerModal = document.getElementById("why-buy-popup");
-  //   var btn = document.getElementById("why-buy-btn");   // trigger
-  //   // UPS Map popup
-  //   var mapModal = document.getElementById("map-popup");
-  //   var mapBtn = document.getElementById("map-btn");    // trigger
-  //   var mapBtn2 = document.getElementById("map-btn2");  // trigger
-  //   // Media Drawer iframe
-  //   var mediaDrawer = document.getElementById("media-drawer");
-  //   var mediaDrwrOverlay = document.getElementById("media-drwr-overlay");
-  //   var mediaBtnDesktop = document.querySelectorAll("#media-btn-desktop");   // iframe trigger only
-  //   var mediaBtnMobile = document.querySelectorAll('#media-btn-mobile');     // acf <a> for link
-  //   var mediaLinkDesktop = [];
-  //   // Recently Viewed popup
-  //   var recentViewModal = document.getElementById("recent-view-popup");
-  //   var recentViewBtn = document.getElementById("recent-view-btn");    // trigger
-  
-  //   Object.keys(mediaBtnDesktop).forEach((key) => {
-  //     mediaLinkDesktop[key] = mediaBtnDesktop[key].getAttribute("data-iframe-link");
-      
-  //     mediaBtnDesktop[key].onclick = function() {
-  //       var mediaIframe = document.getElementById('media-iframe');
-  //       mediaIframe.setAttribute('src', mediaLinkDesktop[key]);
-  //       mediaDrawer.style.display = "block";
-  //       mediaDrawer.classList.add('drawer--is-open');
-  //       mediaDrwrOverlay.style.display = "block";
-  //     }
-  //   });
-  
-  //   if (btn != null){
-  //     btn.onclick = function() {
-  //       outerModal.style.display = "flex";
-  //     }
-  //   }
-  
-  //   if (mapBtn != null){
-  //     mapBtn.onclick = function() {
-  //       mapModal.style.display = "flex";
-  //     }
-  //   }
-  
-  //   if (mapBtn2 != null){
-  //     mapBtn2.onclick = function() {
-  //       mapModal.style.display = "flex";
-  //     }
-  //   }
-  
-  //   if (recentViewBtn != null) {
-  //     if (Object.keys(theme.recentlyViewed.recent).length <= 1 && theme.recentlyViewed.recent.constructor === Object) {
-  //       // No previous history on page load, so bail & don't display modal
-  //       return;
-  //     }
-  //     recentViewBtn.onclick = function() {
-  //       recentViewModal.style.display = "flex";
-  //     }
-  //   }
-  
-  //   // toggles display of iframe and <a> link if both are present in acf metafields
-  //   if (mediaBtnDesktop != null && mediaBtnMobile != null){
-  //     if(/Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent)){
-  //       mediaBtnMobile.forEach(el => el.style.display = "inline-block");
-  //       mediaBtnDesktop.forEach(el => el.style.display = "none");
-  //     }else{
-  //       mediaBtnMobile.forEach(el => el.style.display = "none");
-  //       mediaBtnDesktop.forEach(el => el.style.display = "inline-block");
-  //     }
-  //   }
-  
-  //   document.addEventListener(
-  //     "click",
-  //     function(event) {
-  //       // If user either clicks X button OR clicks outside the modal window, then close modal 
-  //       if (
-  //         event.target.matches(".close-why-popup") 
-  //         ||
-  //         !event.target.closest("#why-modal-inner") &&
-  //         event.target.matches("#why-outer-modal") 
-  //         ||
-  //         !event.target.closest("#why-modal-inner") &&
-  //         event.target.matches("#media-drwr-overlay") 
-  //       ) {
-  //         closeModal()
-  //       }
-  //     },
-  //     false
-  //   )
-     
-  //   function closeModal() {
-  //     if (outerModal != null) {
-  //       outerModal.style.display = "none";
-  //     }
-  //     if (mapModal != null) {
-  //       mapModal.style.display = "none";
-  //     }
-  //     if (mediaDrawer != null && mediaDrwrOverlay != null) {
-  //       mediaDrawer.style.display = "none";
-  //       mediaDrwrOverlay.style.display = "none";
-  //     }
-  //     if (recentViewModal != null) {
-  //       recentViewModal.style.display = "none";
-  //     }
-  //   }
-  // }
-  // customModals();
-
 
 })();
